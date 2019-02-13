6d9b9a6319f5600cd36b1f161ef21364e6b940ff
admin <admin@20a>
2019-02-13 11:42:29 +0700

    create kelas dokumen

---
+++ DocumentRetrieval/src/model/Document.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author puspaingtyas
+ */
+public class Document {
+    private String content;
+
+    public Document() {
+    }
+
+    public Document(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+}
