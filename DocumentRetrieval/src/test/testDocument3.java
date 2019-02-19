c87063a20c969575b0bf562c173ce39a6566ee1d
puspaningtyas <>
2019-02-19 12:46:55 +0700

    buat posting list from list of document

--- DocumentRetrieval/src/test/testDocument3.java
+++ DocumentRetrieval/src/test/testDocument3.java
@@ -15,6 +15,7 @@ import model.Posting;
  * @author admin
  */
 public class testDocument3 {
+
     public static void main(String[] args) {
         // seting dokumen
         Document doc1 = new Document(1, "computer information retrieval.");
@@ -25,12 +26,22 @@ public class testDocument3 {
         // siapkan posting List
         ArrayList<Posting> list = new ArrayList<Posting>();
         // buat node Posting utk listofdocument
-        
-        
+        for (int i = 0; i < listOfDocument.size(); i++) {
+            // buat listOfTerm dari document ke -i
+            String[] termResult = listOfDocument.get(i).getListofTerm();
+            // loop sebanyak term dari document ke i
+            for (int j = 0; j < termResult.length; j++) {
+                // buat object tempPosting
+                Posting tempPosting = new Posting(termResult[j],
+                        listOfDocument.get(i));
+                list.add(tempPosting);
+            }
+        }
+
         // panggil list posting
-        System.out.println("Ukuran list = "+list.size());
+        System.out.println("Ukuran list = " + list.size());
         for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i).getTerm()+","+list.get(i).getDocument().getId());
+            System.out.println(list.get(i).getTerm() + "," + list.get(i).getDocument().getId());
         }
     }
 }
