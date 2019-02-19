6e90279d2a86779a340780d7926336e2af61c58c
user only <user only@DESKTOP-AEGLGP4>
2019-02-19 13:00:32 +0700

    buat posting list from list of document

---
+++ DocumentRetrieval/src/test/testDocument4.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Document;
+import model.InvertedIndex;
+import model.Posting;
+
+/**
+ *
+ * @author admin
+ */
+public class testDocument4 {
+
+    public static void main(String[] args) {
+        // seting dokumen
+        Document doc1 = new Document(1, "computer information retrieval.");
+        Document doc2 = new Document(2, "computer organization and architecture");
+        Document doc3 = new Document(3, "machine learning architecture");
+        
+        // buat object invertedIndex
+        InvertedIndex index = new InvertedIndex();
+        // tmbahkan document ke index
+        index.addNewDocument(doc1);
+        index.addNewDocument(doc2);
+        index.addNewDocument(doc3);
+        // panggil function unsorted Posting List
+        ArrayList<Posting> list = index.getUnsortedPostingList();
+        // panggil list posting
+        System.out.println("Ukuran list = " + list.size());
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getTerm() + "," + list.get(i).getDocument().getId());
+        }
+    }
+}
