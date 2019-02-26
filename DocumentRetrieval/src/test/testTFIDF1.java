386d589c28c0be1ba14373608c195eff6811cb1d
user only <user only@DESKTOP-AEGLGP4>
2019-02-26 13:46:04 +0700

    searching query

---
+++ DocumentRetrieval/src/test/testTFIDF1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Document;
+import model.InvertedIndex;
+import model.Posting;
+import model.Term;
+
+/**
+ *
+ * @author admin
+ */
+public class testTFIDF1 {
+
+    public static void main(String[] args) {
+        // seting dokumen
+        Document doc1 = new Document(1, "computer information retrieval.");
+        Document doc2 = new Document(2, "computer organization and architecture");
+        Document doc3 = new Document(3, "machine learning inteligence architecture");
+        Document doc4 = new Document(4, "machine learning artificial inteligence");
+        
+        // buat object invertedIndex
+        InvertedIndex index = new InvertedIndex();
+        // tmbahkan document ke index
+        index.addNewDocument(doc1);
+        index.addNewDocument(doc2);
+        index.addNewDocument(doc3);
+        index.addNewDocument(doc4);
+        // panggil fungsi search
+        index.makeDictionary();
+        ArrayList<Posting> result = index.search("machine learning inteligence");
+        // tampilkan isi document dan id-nya
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println("id_doc = " +result.get(i).getDocument().getId());
+            System.out.println(result.get(i).getDocument().getContent());
+        }
+        
+    }
+}
