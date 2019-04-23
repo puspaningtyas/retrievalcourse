56e195de2e8cd8be9dc5d08ea4cf6ac0b32dc7b4
admin <admin@dosenC>
2019-04-23 12:07:08 +0700

    test stopword in lucene

--- DocumentRetrieval/src/testLucene/TestStopWordToken.java
+++ DocumentRetrieval/src/testLucene/TestStopWordToken.java
@@ -13,8 +13,8 @@ import org.apache.lucene.analysis.core.StopFilter;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.util.Version;
-
 
 /**
  *
@@ -23,7 +23,8 @@ import org.apache.lucene.util.Version;
 public class TestStopWordToken {
 
     public static void main(String[] args) {
-        String text = "I want to break my freedom";
+        String text = "He was a man with gun";
+        System.out.println("Text = "+text);
         Version matchVersion = Version.LUCENE_7_7_0; // Substitute desired Lucene version for XY
         Analyzer analyzer = new StandardAnalyzer();
         analyzer.setVersion(matchVersion);
@@ -31,9 +32,23 @@ public class TestStopWordToken {
         CharArraySet stopWords = EnglishAnalyzer.getDefaultStopSet();
         // buat token
         TokenStream tokenStream = analyzer.tokenStream(
-                "myField", 
+                "myField",
                 new StringReader(text.trim()));
         // buang stop word
         tokenStream = new StopFilter(tokenStream, stopWords);
+        // buat string baru tanpa stopword
+        StringBuilder sb = new StringBuilder();
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+        try {
+            tokenStream.reset();
+            while (tokenStream.incrementToken()) {
+                String term = charTermAttribute.toString();
+                sb.append(term + " ");
+            }
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
+        String newText = sb.toString();
+        System.out.println("New Text = "+newText);
     }
 }
