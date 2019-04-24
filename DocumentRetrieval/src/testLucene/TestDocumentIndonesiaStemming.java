ff33ad24fd8f5e2881b0628befdc6abf15adccac
admin <admin@20a>
2019-04-24 11:06:44 +0700

    buat project test web search page

--- DocumentRetrieval/src/testLucene/TestDocumentIndonesiaStemming.java
+++ DocumentRetrieval/src/testLucene/TestDocumentIndonesiaStemming.java
@@ -13,7 +13,8 @@ import model.Document;
  */
 public class TestDocumentIndonesiaStemming {
     public static void main(String[] args) {
-        Document doc = new Document(1,"Dia sedang pergi berbelanja di pusat perbelanjaan.");
+        Document doc = new Document(1,"Dia sedang pergi berbelanja di pusat perbelanjaan."
+                + "Namun, ibunya melarangnya pergi kesana karena tempat itu sangat berbahaya.");
         doc.IndonesiaStemming();
         System.out.println(doc);
     }
